a54b6fab54c6a52908bcae823e8900092a100842
Arseny Gabdullin <me@arsgab.io>
2025-04-17 19:15:58 +0200

    Properly setup domain names

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig, envField } from "astro/config";
 
-const SITE_URL = process.env.SITE_URL || "https://niti-space.github.io";
+const ORG_NAME = process.env.ORG_NAME;
+const SITE_FQDN = process.env.SITE_FQDN || `${ORG_NAME}.github.io`;
+const SITE_BASE_PATH = process.env.SITE_BASE_PATH;
 
 export default defineConfig({
-  site: SITE_URL,
-  base: "site",
+  site: `https://${SITE_FQDN}`,
+  ...(SITE_BASE_PATH ? { base: SITE_BASE_PATH } : {}),
   build: {
     assets: "static",
   },
